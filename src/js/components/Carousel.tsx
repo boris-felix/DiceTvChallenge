9511be8b0fbc6017e71037289f14fcc2e0346986
Ventsislav Dimitrov <vdimitrovv@users.noreply.github.com>
2019-06-27 16:27:26 +0100

    Ed's test

--- src/js/components/Carousel.tsx
+++ src/js/components/Carousel.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-export const Carousel = () => {
-    return null;
-}
+export const Carousel = ({ items }) => {
+    return (
+        <ul>
+            {items.map((item, key) => (
+                <li key={key} style={{ backgroundImage: `url(${item.cover})` }}>
+                    <span className="item-name">{item.name}</span>
+                </li>
+             ))}
+        </ul>
+    );
+};
--- src/js/index.tsx
+++ src/js/index.tsx
@@ -11,12 +11,12 @@ const Sample = [
         cover: 'https://www.thenational.ae/image/policy:1.855106:1556542236/sp30-Khabib-Nurmagomedov.jpg?f=16x9&w=1200&$p$f$w=838e3e4'
     },
     {
-        name: 'Item 1',
+        name: 'Item 2',
         description: 'simple description',
         cover: 'https://www.thenational.ae/image/policy:1.855106:1556542236/sp30-Khabib-Nurmagomedov.jpg?f=16x9&w=1200&$p$f$w=838e3e4'
     },
     {
-        name: 'Item 1',
+        name: 'Item 3',
         description: 'simple description',
         cover: 'https://www.thenational.ae/image/policy:1.855106:1556542236/sp30-Khabib-Nurmagomedov.jpg?f=16x9&w=1200&$p$f$w=838e3e4'
     }
