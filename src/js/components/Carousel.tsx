1fdc1878ba3821a53ed48004d52b18bfbc8906fb
Ventsislav Dimitrov <vdimitrovv@users.noreply.github.com>
2019-07-03 19:02:00 +0100

    gary's changes

--- src/js/components/Carousel.tsx
+++ src/js/components/Carousel.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
-export const Carousel = () => {
-    return null;
+export const Carousel = (props) => {
+
+    return <article className='carousel'> {props.items.map( item => (
+        <div class='carousel__col'>
+            <h2>{item.name}</h2>
+
+            <p>{item.description}</p>
+
+            <img className='carousel__image' src={item.cover} />
+        </div>)
+    )}</article>;
 }
--- src/js/index.tsx
+++ src/js/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import  { Carousel } from './components/Carousel';
 
-import '../sass/_index.scss';
+import '../sass/index.scss';
 
 const Sample = [
     {
@@ -21,6 +21,7 @@ const Sample = [
         cover: 'https://www.thenational.ae/image/policy:1.855106:1556542236/sp30-Khabib-Nurmagomedov.jpg?f=16x9&w=1200&$p$f$w=838e3e4'
     }
 ]
+
 const App = (props: any) => {
     return <Carousel items={Sample} />;
 }
