0868588d7703e00418a822f67bb5edd4f3977c51
Ventsislav Dimitrov <vdimitrovv@users.noreply.github.com>
2019-07-11 12:17:48 +0100

    ben's test

--- src/js/components/Carousel.tsx
+++ src/js/components/Carousel.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
-export const Carousel = () => {
-    return null;
+export const Carousel = (props: any) => {
+    return (
+        <div className="carousel">
+            {props.items.map((item, index) => {
+                return (
+                    <div className="container" key={index}>
+                        <img className="carousel__image" src={item.cover}/>
+                        <div className="carousel__content">
+                            <p>{item.name}</p>
+                            <p>{item.description}</p>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
